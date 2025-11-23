d11380b73276b3ffe3e84c4bf390af935aa097c1
tuxikus <contact@tuxikus.de>
2025-11-23 19:41:34 +0100

    first usable version

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
+	"os/exec"
 	"path"
-	"path/filepath"
+	"runtime"
 	"strconv"
 )
 
@@ -15,14 +17,45 @@ type Bookmark struct {
 	Tags []string `json:"tags"`
 }
 
-func getDefaultBookmarskFilePath() (string, error) {
-	// home, err := os.UserHomeDir()
-	// if err != nil {
-	// 	return "", err
-	// }
+func printHelp() {
+	helpText := `usage: 
+	$ bit <command> <args>
 
-	// return path.Join(home, ".bookmarks.json"), nil
-	return path.Join(".", "test.json"), nil
+commands:
+	add <name> <link> <tags> - add a bookmark
+	list - list all bookmarks
+	delete <index> - delete a bookmark
+	open <index> - open a bookmark
+
+	help - print this message
+	`
+
+	fmt.Println(helpText)
+}
+
+func getDefaultBookmarksFilePath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	return path.Join(home, ".bookmarks.json"), nil
+}
+
+func createDefaultBookmarksFile() error {
+	path, err := getDefaultBookmarksFilePath()
+	if err != nil {
+		return err
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		_, err = os.Create(path)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func store(bookmarks []Bookmark) error {
@@ -31,7 +64,11 @@ func store(bookmarks []Bookmark) error {
 		return err
 	}
 
-	path := filepath.Join(".", "test.json")
+	path, err := getDefaultBookmarksFilePath()
+	if err != nil {
+		return err
+	}
+
 	f, err := os.Create(path)
 	if err != nil {
 		return err
@@ -39,29 +76,28 @@ func store(bookmarks []Bookmark) error {
 
 	defer f.Close()
 
-	// dont need to know how much byte written
 	_, err = f.Write(bmJSON)
 	return err
 }
 
-func listBookmarks(bookmarks []Bookmark) {
-	for i, bm := range bookmarks {
-		fmt.Println(i, bm.Name, bm.Link)
+func getOpenCmd() string {
+	cmd := ""
+
+	switch runtime.GOOS {
+	case "darwin":
+		cmd = "open"
+	default:
+		cmd = "xdg-open"
 	}
+
+	return cmd
 }
 
-func load(bookmarks *[]Bookmark) error {
-	path, err := getDefaultBookmarskFilePath()
-	if err != nil {
-		return err
-	}
+func openBookmark(bookmarks []Bookmark, idx int) error {
+	cmd := getOpenCmd()
+	link := bookmarks[idx].Link
 
-	content, err := os.ReadFile(path)
-	if err != nil {
-		return err
-	}
-
-	err = json.Unmarshal(content, &bookmarks)
+	err := exec.Command(cmd, link).Start()
 	if err != nil {
 		return err
 	}
@@ -69,12 +105,47 @@ func load(bookmarks *[]Bookmark) error {
 	return nil
 }
 
-func addBookmark(bookmarks *[]Bookmark, bookmark Bookmark) {
-	*bookmarks = append(*bookmarks, bookmark)
+func listBookmarks(bookmarks []Bookmark) {
+	for i, bm := range bookmarks {
+		fmt.Println(i, bm.Name, bm.Link, bm.Tags)
+	}
 }
 
-func deleteBookmark(bookmarks *[]Bookmark, idx int) {
-	*bookmarks = append((*bookmarks)[:idx], (*bookmarks)[idx+1:]...)
+func load() ([]Bookmark, error) {
+	path, err := getDefaultBookmarksFilePath()
+	if err != nil {
+		return nil, err
+	}
+
+	content, err := os.ReadFile(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []Bookmark{}, nil
+		}
+		return nil, err
+	}
+
+	if len(content) == 0 || string(content) == "" {
+		return []Bookmark{}, nil
+	}
+
+	var bookmarks []Bookmark
+	err = json.Unmarshal(content, &bookmarks)
+	if err != nil {
+		return nil, err
+	}
+
+	return bookmarks, nil
+}
+
+func addBookmark(bookmarks []Bookmark, bookmark Bookmark) []Bookmark {
+	bookmarks = append(bookmarks, bookmark)
+	return bookmarks
+}
+
+func deleteBookmark(bookmarks []Bookmark, idx int) []Bookmark {
+	bookmarks = append(bookmarks[:idx], bookmarks[idx+1:]...)
+	return bookmarks
 }
 
 func buildBookmark() Bookmark {
@@ -84,7 +155,7 @@ func buildBookmark() Bookmark {
 
 	// got tags
 	if len(os.Args) > 3 {
-		tags = append(tags, os.Args[3:]...)
+		tags = append(tags, os.Args[4:]...)
 	}
 
 	return Bookmark{
@@ -95,28 +166,66 @@ func buildBookmark() Bookmark {
 }
 
 func main() {
-	bookmarks := make([]Bookmark, 0)
-	err := load(&bookmarks)
+	err := createDefaultBookmarksFile()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(
+			errors.Join(errors.New("[ERROR] unable to create bookmarks file"), err),
+		)
+	}
+
+	bookmarks, err := load()
+	if err != nil {
+		fmt.Println(
+			errors.Join(errors.New("[ERROR] unable to load bookmarks from file"), err),
+		)
+		bookmarks = []Bookmark{}
+	}
+
+	if len(os.Args) < 2 {
+		fmt.Println("incorrect argument count")
+		printHelp()
+		os.Exit(1)
 	}
 
 	switch os.Args[1] {
 	case "add":
-		addBookmark(&bookmarks, buildBookmark())
+		bookmarks = addBookmark(bookmarks, buildBookmark())
 	case "list":
 		listBookmarks(bookmarks)
 	case "delete":
 		idx, err := strconv.Atoi(os.Args[2])
 		if err != nil {
+			fmt.Println(
+				errors.Join(errors.New("[ERROR] unable to parse index to int"), err),
+			)
 			fmt.Println(err)
 			os.Exit(-1)
 		}
-		deleteBookmark(bookmarks, idx)
+		bookmarks = deleteBookmark(bookmarks, idx)
+	case "open":
+		idx, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Println(
+				errors.Join(errors.New("[ERROR] unable to parse index to int"), err),
+			)
+			os.Exit(-1)
+		}
+
+		err = openBookmark(bookmarks, idx)
+		if err != nil {
+			fmt.Println(
+				errors.Join(errors.New("[ERROR] unable to open bookmark"), err),
+			)
+			os.Exit(-1)
+		}
+	default:
+		printHelp()
 	}
 
 	err = store(bookmarks)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(
+			errors.Join(errors.New("[ERROR] unable to store bookmarks"), err),
+		)
 	}
 }
